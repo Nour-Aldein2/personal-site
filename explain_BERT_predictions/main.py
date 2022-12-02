ab17aa21bccbc744f26e9a047156e3615ece6dac
Nour-Aldein2 <83635963+Nour-Aldein2@users.noreply.github.com>
2022-12-02 17:19:24 +0400

    Update main.py

--- explain_BERT_predictions/main.py
+++ explain_BERT_predictions/main.py
@@ -147,8 +147,8 @@ with header:
 with dataset:
     st.header("Tweets Classification")
 
-    df_train = load_data("./train.csv")
-    df_test = load_data("./test.csv")
+    df_train = load_data("personal-site/explain_BERT_predictions/train.csv")
+    df_test = load_data("personal-site/explain_BERT_predictions/test.csv")
 
     if st.checkbox("Show training data:"):
         data_load_state = st.text('Loading data...')
