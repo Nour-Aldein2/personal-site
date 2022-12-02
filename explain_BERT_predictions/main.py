effbd0426f3d4f3edce549b35f3ee58454828f62
Nour-Aldein2 <83635963+Nour-Aldein2@users.noreply.github.com>
2022-12-02 17:49:31 +0400

    Update main.py

--- explain_BERT_predictions/main.py
+++ explain_BERT_predictions/main.py
@@ -146,10 +146,12 @@ with header:
 ####################################################################
 with dataset:
     st.header("Tweets Classification")
-    
-    
-    df_test = load_data(r".\\nlp-getting-started\\test.csv")
-    df_train = load_data(r".\\nlp-getting-started\\train.csv")
+    try:
+        df = pd.read_csv("train.csv")
+        st.write(df)
+
+    df_test = load_data(r"/nlp-getting-started/test.csv")
+    df_train = load_data(r"/nlp-getting-started/train.csv")
     
 
     if st.checkbox("Show training data:"):
