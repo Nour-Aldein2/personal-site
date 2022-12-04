b87b08e860965a1ded57cb7bfce9d83911323f13
Nour-Aldein2 <nour.aldein002.2@gmail.com>
2022-12-05 00:04:05 +0400

    add dropdown menu

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,6 +3,7 @@ from dash import html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 
 import pandas as pd
+import numpy as np
 
 import plotly.express as px
 
@@ -18,67 +19,14 @@ Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
 if this is your first introduction to Markdown!
 '''
 
-## Python 
+# Python
 df_train = text_processing.load_and_process_data("train.csv")
 df_test = text_processing.load_and_process_data("test.csv")
-
-
-# Dashboard
-def generate_table(dataframe, max_rows=10):
-    return dash.dash_table.DataTable(
-        data=dataframe.to_dict('records'),
-        columns=[{'id': c, 'name': c} for c in dataframe.columns],
-
-        style_cell_conditional=[
-            {
-                'if': {'column_id': c},
-                'textAlign': 'left',
-            } for c in ['id', 'text']
-        ],
-        style_data={
-            'color': 'black',
-            'textAlign': 'left',
-            'overflow': 'auto',
-            'backgroundColor': 'white',
-        },
-        style_data_conditional=[
-            {
-                'if': {'row_index': 'odd'},
-                'textAlign': 'left',
-                'backgroundColor': 'rgb(245,245,245)',
-            }
-        ],
-        style_header={
-            'backgroundColor': 'rgb(224,224,224)',
-            'color': 'black',
-            'textAlign': 'left',
-            'fontWeight': 'bold',
-            'position':'sticky',
-            'top': '0'
-        },
-        style_table={
-            'height': 400,
-            'overflowY': 'scroll',
-            'overflowX': 'scroll',
-            'width': 'relative'
-        }
-    )
 
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
-feature = "char_count"
-
-fig_train = px.histogram(df_train, x=feature, marginal="box", color_discrete_sequence=['turquoise'])
-fig_train.update_layout(height=600)
-fig_test = px.histogram(df_test, x=feature, marginal="box", color_discrete_sequence=['indianred'])
-fig_test.update_layout(height=600)
-
-colors = {
-    'background': '#111111',
-    'text': '#7FDBFF'
-}
 
 
 app.layout = html.Div(children=[
@@ -87,80 +35,103 @@ app.layout = html.Div(children=[
         children='Explaining USE Predictions with LIME',
         style={
             "textAlign": "center",
-            "color": "#2BBBB" #colors["text"]
+            "color": "#2BBBB"  # colors["text"]
         }
     ),
 
     html.H2("Overview"),
     dcc.Markdown(children=markdown_text),
+    dcc.Dropdown(
+        df_test.select_dtypes(include=np.number).columns[1:],  # Don't include ID
+        "char_count",
+        id="feature-selector"
+        ),
 
-    # Training Data
+    # ----------------------- Training Data
     html.Div([
         dbc.Row([
             dbc.Col(
                 dbc.Container(
                     dbc.Card(
                         html.Div(
-                            dbc.Table.from_dataframe(df_train[["id","text","target"]]),
+                            dbc.Table.from_dataframe(
+                                df_train[["id", "text", "target"]]),
                             style={"maxHeight": "450px", "overflow": "scroll"},
                         ),
                         body=True,
-                    ), 
+                    ),
                     className="p-5",
-                ), width={"size":6, "order":"first"}
+                ), width={"size": 6, "order": "first"}
             ),
             dbc.Col(
                 html.Div([
-                    dcc.Graph(
-                        id="Train Data",
-                        figure=fig_train,
-                        # style={'padding':10}
-                    )
-                ]), width={"size":6, "order":"last"},
+                    dcc.Graph(id="train-hist")
+                ]), width={"size": 6, "order": "last"},
             )
         ]),
     ],
     ),
 
-
+    # ----------------------- Testing Data
     html.Div([
         dbc.Row([
             dbc.Col(
                 dbc.Container(
                     dbc.Card(
                         html.Div(
-                            dbc.Table.from_dataframe(df_test[["id","text"]]),
+                            dbc.Table.from_dataframe(df_test[["id", "text"]]),
                             style={"maxHeight": "450px", "overflow": "scroll"},
                         ),
                         body=True,
-                    ), 
+                    ),
                     className="p-5",
-                ), width={"size":6, "order":"first"}
+                ), width={"size": 6, "order": "first"}
             ),
             dbc.Col(
                 html.Div([
-                    dcc.Graph(
-                        id="Test Data",
-                        figure=fig_test,
-                        # style={'padding':10}
-                    )
-                ]), width={"size":6, "order":"last"},
+                    dcc.Graph(id="test-hist")
+                ]), width={"size": 6, "order": "last"},
             )
         ]),
     ],
     ),
 
 
-    
 
 
-# Input yout text
+
+    # Input yout text
     html.Div([
         html.Label('Text Input'),
         dcc.Input(value='MTL', type='text'),
     ]),
 
-], style={"padding":"5%"})
+], style={"padding": "5%"})
+
+
+
+
+
+@app.callback(
+    Output(component_id='train-hist', component_property='figure'),
+    Input(component_id='feature-selector', component_property='value')
+)
+def update_figure(feature):
+    fig_train = px.histogram(df_train, x=feature, marginal="box",
+                         color_discrete_sequence=['turquoise'])
+    fig_train.update_layout(height=600)
+    return fig_train
+
+@app.callback(
+    Output(component_id='test-hist', component_property='figure'),
+    Input(component_id='feature-selector', component_property='value')
+)
+def update_figure(feature):
+    fig_test = px.histogram(df_test, x=feature, marginal="box",
+                            color_discrete_sequence=['indianred'])
+    fig_test.update_layout(height=600)
+    return fig_test
+
 
 if __name__ == "__main__":
     app.run(debug=True)
