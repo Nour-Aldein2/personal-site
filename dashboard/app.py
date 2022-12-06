8388c10f250d56a7d73879d47c25ad2f3d72ff70
Nour-Aldein2 <nour.aldein002.2@gmail.com>
2022-12-06 21:46:07 +0400

    fix bug

--- dashboard/app.py
+++ dashboard/app.py
@@ -20,110 +20,84 @@ if this is your first introduction to Markdown!
 '''
 
 # Python
+print("Start app!")
 df_train = text_processing.load_and_process_data("train.csv")
+print("Got train data")
 df_test = text_processing.load_and_process_data("test.csv")
+print("Got test data")
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+BS = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+app = dash.Dash(external_stylesheets=[BS])
+print("created the app")
 
-app.layout = html.Div(children={
-
-    html.H1(
-        children='Explaining USE Predictions with LIME',
-        style={
-            "textAlign": "center",
-            "color": "#2BBBB"  # colors["text"]
-        }
-    ),
-
-    # html.H2("Overview"),
-    # dcc.Markdown(children=markdown_text),
-    # dcc.Dropdown(
-    #     df_test.select_dtypes(include=np.number).columns[1:],  # Don't include ID
-    #     "char_count",
-    #     id="feature-selector"
-    # ),
-
-    # # ----------------------- Training Data
-    # html.Div([
-    #     dbc.Row([
-    #         dbc.Col(
-    #             dbc.Container(
-    #                 dbc.Card(
-    #                     html.Div(
-    #                         dbc.Table.from_dataframe(
-    #                             df_train[["id", "text", "target"]]),
-    #                         style={"maxHeight": "450px", "overflow": "scroll"},
-    #                     ),
-    #                     body=True,
-    #                 ),
-    #                 className="p-5",
-    #             ), width={"size": 6, "order": "first"}
-    #         ),
-    #         dbc.Col(
-    #             html.Div([
-    #                 dcc.Graph(id="train-hist")
-    #             ]), width={"size": 6, "order": "last"},
-    #         )
-    #     ]),
-    # ],
-    # ),
-
-    # # ----------------------- Testing Data
-    # html.Div([
-    #     dbc.Row([
-    #         dbc.Col(
-    #             dbc.Container(
-    #                 dbc.Card(
-    #                     html.Div(
-    #                         dbc.Table.from_dataframe(df_test[["id", "text"]]),
-    #                         style={"maxHeight": "450px", "overflow": "scroll"},
-    #                     ),
-    #                     body=True,
-    #                 ),
-    #                 className="p-5",
-    #             ), width={"size": 6, "order": "first"}
-    #         ),
-    #         dbc.Col(
-    #             html.Div([
-    #                 dcc.Graph(id="test-hist")
-    #             ]), width={"size": 6, "order": "last"},
-    #         )
-    #     ]),
-    # ],
-    # ),
-
-
-    # Input your text
-    html.Div([
-        html.Label('Text Input'),
-        dcc.Input(value='MTL', type='text'),
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1("Explaining USE with LIME"), width=12)
     ]),
-}, style={"padding": "5%"})
+    dbc.Row([
+        dcc.Dropdown(
+            df_test.select_dtypes(include=np.number).columns[1:],  # Don't include ID
+            "char_count",
+            id="feature-selector"
+        ),
+    ]),
+    # ----------------------- Training Data
+    dbc.Row([
+        dbc.Col(
+            dbc.Container(
+                dbc.Card(
+                    html.Div(
+                        dbc.Table.from_dataframe(
+                            df_train[["id", "text", "target"]]),
+                        style={"maxHeight": "450px", "overflow": "scroll"},
+                    ), body=True,
+                ), className="p-5"), width={"size": 6, "order": "first"}
+        ),
+        dbc.Col(
+            html.Div([
+                dcc.Graph(id="train-hist")
+            ]), width={"size": 6, "order": "last"},
+        )
+    ]),
+    # ----------------------- Testing Data
+    dbc.Row([
+        dbc.Col(
+            dbc.Container(
+                dbc.Card(
+                    html.Div(
+                        dbc.Table.from_dataframe(df_test[["id", "text"]]),
+                        style={"maxHeight": "450px", "overflow": "scroll"},
+                    ), body=True,
+                ), className="p-5"), width={"size": 6, "order": "first"}
+        ),
+        dbc.Col(
+            html.Div([
+                dcc.Graph(id="test-hist")
+            ]), width={"size": 6, "order": "last"},
+        )
+    ]),
+], className="m-5", style={'backgroundColor': 'var(--bs-light)', })
+
+@app.callback(
+    Output(component_id='train-hist', component_property='figure'),
+    Input(component_id='feature-selector', component_property='value')
+)
+def update_figure(feature):
+    fig_train = px.histogram(df_train, x=feature, marginal="box",
+                             color_discrete_sequence=['turquoise'])
+    fig_train.update_layout(height=600)
+    return fig_train
 
 
-# @app.callback(
-# #     Output(component_id='train-hist', component_property='figure'),
-# #     Input(component_id='feature-selector', component_property='value')
-# # )
-# # def update_figure(feature):
-# #     fig_train = px.histogram(df_train, x=feature, marginal="box",
-# #                              color_discrete_sequence=['turquoise'])
-# #     fig_train.update_layout(height=600)
-# #     return fig_train
+@app.callback(
+    Output(component_id='test-hist', component_property='figure'),
+    Input(component_id='feature-selector', component_property='value')
+)
+def update_figure(feature):
+    fig_test = px.histogram(df_test, x=feature, marginal="box",
+                            color_discrete_sequence=['indianred'])
+    fig_test.update_layout(height=600)
+    return fig_test
 
 
-# # @app.callback(
-# #     Output(component_id='test-hist', component_property='figure'),
-# #     Input(component_id='feature-selector', component_property='value')
-# # )
-# # def update_figure(feature):
-# #     fig_test = px.histogram(df_test, x=feature, marginal="box",
-# #                             color_discrete_sequence=['indianred'])
-# #     fig_test.update_layout(height=600)
-# #     return fig_test
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+app.run(debug=True)
--- dashboard/playground.py
+++ dashboard/playground.py
@@ -1,113 +1,49 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, html, dcc, Input, Output
+import dash
+from dash import dcc, html
 import dash_bootstrap_components as dbc
-import plotly.express as px
-import pandas as pd
-import text_processing
-import numpy as np
 
-app = Dash(
-    __name__, 
-    external_stylesheets=[dbc.themes.BOOTSTRAP],
-    suppress_callback_exceptions=True)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-# Load data
-df_train = text_processing.load_and_process_data("./train.csv")
-df_test = text_processing.load_and_process_data("./test.csv")
-
-features = df_test.select_dtypes(include=np.number).columns[1:]
-fig_train = px.histogram(df_train, x="char_count", marginal="box",
-                             color_discrete_sequence=['turquoise'])
-
-
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-app.layout = html.Div(children={
-
-    html.H1(
-        children='Explaining USE Predictions with LIME',
-        style={
-            "textAlign": "center",
-            "color": "#2BBBB"  # colors["text"]
-        }
-    ),
-
-    html.H2("Overview"),
-    dcc.Dropdown(
-        features,  # Don't include ID
-        "char_count",
-        id="feature-selector"
-    ),
-    # ----------------------- Training Data
-    html.Div([
-        dbc.Row([
-            dbc.Col(
-                dbc.Container(
-                    dbc.Card(
-                        html.Div(
-                            dbc.Table.from_dataframe(
-                                df_train[["id", "text", "target"]]),
-                            style={"maxHeight": "450px", "overflow": "scroll"},
-                        ),
-                        body=True,
-                    ),
-                    className="p-5",
-                ), width={"size": 6, "order": "first"}
-            ),
-            dbc.Col(
-                html.Div([
-                    dcc.Graph(id="train-hist")
-                ]), width={"size": 6, "order": "last"},
-            )
-        ]),
+graph = dcc.Graph(figure={
+    'data': [
+        {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+        {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
     ],
-    ),
+    'layout': {
+        'title': 'Dash Data Visualization',
+    },
+}, responsive=True, className="h-100")
 
-    # ----------------------- Testing Data
-    html.Div([
-        dbc.Row([
-            dbc.Col(
-                    html.Div([
-                        dbc.Table.from_dataframe(df_test[["id", "text"]]),
-                        # style={"maxHeight": "450px", "overflow": "scroll"},
-                    ]), width={"size": 6, "order": "first"}
-                    ),
-            dbc.Col(
-                html.Div([
-                    dcc.Graph(id="test-hist")
-                ]), width={"size": 6, "order": "last"},
-            )
-        ]),
-    ]),
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.Div("stuff", className="bg-secondary h-100"), width=2),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")], width=4)
+            ]),
+            dbc.Row([
+                dbc.Col([graph]),
+            ]),
+            dbc.Row([
+                dbc.Col([graph]),
+            ]),
+        ], width=5),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")]),
+                dbc.Col([html.Div("header", className="bg-primary")]),
+            ]),
+            dbc.Row([
+                dbc.Col([graph]),
+            ], className="h-100"),
+        ], width=5),
+    ])
+], fluid=True)
 
-}, style={"padding": "5%"})
-
-
-
-@app.callback(
-    Output(component_id='train-hist', component_property='figure'),
-    Input(component_id='feature-selector', component_property='value')
-)
-def update_figure(feature):
-    fig_train = px.histogram(df_train, x=feature, marginal="box",
-                             color_discrete_sequence=['turquoise'])
-    fig_train.update_layout(height=600)
-    return fig_train
-
-
-@app.callback(
-    Output(component_id='test-hist', component_property='figure'),
-    Input(component_id='feature-selector', component_property='value')
-)
-def update_figure(feature):
-    fig_test = px.histogram(df_test, x=feature, marginal="box",
-                            color_discrete_sequence=['indianred'])
-    fig_test.update_layout(height=600)
-    return fig_test
-
-if __name__ == '__main__':
-    app.run_server(debug=True, port=8080)
+app.run_server(debug=True)
